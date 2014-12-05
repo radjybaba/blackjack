50a101ced484b12e994862a303c337352e0e06bf
radjybaba <geryes@gmail.com>
2014-12-05 15:30:57 +0200

    h

---
+++ blackjack/src/model/test2.java
@@ -0,0 +1,14 @@
+package model;
+
+public class test2 {
+
+	public test2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ blackjack/src/view/test3.java
@@ -0,0 +1,14 @@
+package view;
+
+public class test3 {
+
+	public test3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
