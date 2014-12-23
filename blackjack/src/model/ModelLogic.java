b1ae8bcc89ba69857b78b449640157ebe32cb7d7
radjybaba <geryes@gmail.com>
2014-12-23 19:01:40 +0200

    model logic class 2nd iteration

--- blackjack/src/model/ModelLogic.java
+++ blackjack/src/model/ModelLogic.java
@@ -1,9 +1,24 @@
+/*
+ * 
+ * 	Nadine Smair		308573252			Anton Anton		203323902
+ *	Ameer Dow			203844956			Geryes Moussa 	301622635
+ *
+ *
+ */
+
 package model;
+
+
 
 public class ModelLogic {
 	
 	
     private static Deck cards;
+    private static Game game;
+//    private User user;
+    
+    
+    
 	
     /* 
 	 * returns the Deck that got created 
@@ -31,10 +46,88 @@ public class ModelLogic {
 	public Card getCard(int i){
     	return cards.returnCard(i);
     }
+	
+	/*
+	 * starts new round
+	 */
+	
+	public void newRound(){
+		game.newRound();
+		
+	}
+	
+	/*
+	 * returns that i-th card of specific hand
+	 */
+	
+	public Card returnCard(Hand hand , int i){
+		return hand.getCard(i);
+	}
+	
+	/*
+	 * returns player's hand
+	 */
+	
+	public Hand getPlayerHand(){
+		return game.getRound().getPlayer().getHand();
+	}
+
+	
+	/*
+	 * returns dealer's hand
+	 */
+	
+	public Hand getDealerHand(){
+		return game.getRound().getDealer().getHand();
+	}
+	
+	/*
+	 * returns the player's current hand score
+	 */
+	
+	public int getPlayerCurrentScore(){
+		return game.getRound().getPlayer().getCurrentScore();
+	}
+	
+	/*
+	 * returns the dealer's current hand score
+	 */
+	
+	public int getDealerCurrentScore(){
+		return game.getRound().getDealer().getCurrentScore();
+	}
+	
+	/*
+	 * hits the player with one card
+	 */
+	
+	public void hit(){
+		game.hit();		
+	}
+	
+	/*
+	 * the player chooses to stand and the dealer get cards
+	 */
+	
+	public void stand(){
+		game.stand();
+	}
+	
+	/*
+	public boolean signUp(String username, String password){
+    	return User.signUp(new User(username, password));
+    }
+	
+	public boolean login(String username , String password){
+		return !((this.user = User.login(username, password)).equals(null));
+	}
+
+*/
 
 	public ModelLogic() {
 		// TODO Auto-generated constructor stub
 		cards = Deck.getInstance(); 
+		game = new Game();
 	}
 
 	
